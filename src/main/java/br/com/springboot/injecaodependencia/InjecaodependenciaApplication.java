a61ad3d2b72d363a75fbc774ebd0cdf3b7be4733
mmarciano-moon <michel@moonpayment.com.br>
2026-02-07 16:01:21 -0300

    first commit

---
+++ src/main/java/br/com/springboot/injecaodependencia/InjecaodependenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.injecaodependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjecaodependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjecaodependenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot/injecaodependencia/InjecaodependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.injecaodependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjecaodependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
