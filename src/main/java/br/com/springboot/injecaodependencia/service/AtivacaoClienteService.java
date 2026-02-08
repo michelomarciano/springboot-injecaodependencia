ed254ef06e34f1af4c41a5c663ddb16d31415d8e
mmarciano-moon <michel@moonpayment.com.br>
2026-02-08 11:47:31 -0300

    Update AtivacaoClienteService to allow optional Notificador injection

--- src/main/java/br/com/springboot/injecaodependencia/service/AtivacaoClienteService.java
+++ src/main/java/br/com/springboot/injecaodependencia/service/AtivacaoClienteService.java
@@ -10,7 +10,7 @@ public class AtivacaoClienteService {
 
     private Notificador notificador;
 
-    @Autowired
+    @Autowired (required = false)
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
     }
